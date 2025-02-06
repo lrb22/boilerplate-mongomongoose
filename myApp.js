bad576a851b5e50855bf7f3c6d4b598eb6b96b6d
Leah Barnes <lrb7276@gmail.com>
2025-02-06 18:32:12 +0000

    modified:   myApp.js

--- myApp.js
+++ myApp.js
@@ -5,9 +5,12 @@ mongoose.connect(process.env.MONGO_URI, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
 });
-
-let Person;
-
+const personSchema = new mongoose.Schema({
+  name:{type: String, required: true},
+  age: Number,
+  favoriteFood: [String]
+});
+const Person = mongoose.Model("Person",personSchema);
 const createAndSavePerson = (done) => {
   done(null /*, data*/);
 };
